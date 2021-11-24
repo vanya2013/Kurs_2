3b172ef658cebdc24ce11ab4691fa5bac02b85fa
vanya2013 <vanyA2013@GMAIL.COM>
2021-11-24 22:27:04 +0300

    Initial Commit

---
+++ Kurs_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kurs_2
+//
+//  Created by Ivan Bezgin on 24.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
