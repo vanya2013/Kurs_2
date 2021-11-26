dd79beef0e0d281ea17db8c2443879f254112be0
vanya2013 <vanyA2013@GMAIL.COM>
2021-11-26 15:31:45 +0300

    commit to less_2

--- Kurs_2/LoginFormViewController.swift
+++ Kurs_2/LoginFormViewController.swift
@@ -13,14 +13,39 @@ class LoginFormViewController: UIViewController {
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     let toNextView = "fromLoginFormToNextView"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow(_:)), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide(_:)), name: UIResponder.keyboardDidHideNotification, object: nil)
+    }
+    
+    @objc func keyboardDidShow(_ notification: Notification) {
+        let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue
+        guard let keyboardHeight = keyboardSize?.height else { return }
+        
+        let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight, right: 0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc func keyboardDidHide(_ notification: Notification) {
+        let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue
+        guard let keyboardHeight = keyboardSize?.height else { return }
+        
+        let contentInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
     }
     
     
     @IBAction func pressLoginButton(_ sender: UIButton) {
+        
+        self.view.endEditing(true)
         
         guard let login = loginTextField.text,
               let password = passwordTextField.text
